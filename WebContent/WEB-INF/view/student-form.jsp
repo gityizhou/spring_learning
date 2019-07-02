db10b8785ba05ec35f9cdb9d29a68cf3fe13c820
gityizhou <zxc47POI>
2019-07-02 14:58:19 +0800

    form processing - select tag

--- WebContent/WEB-INF/view/student-form.jsp
+++ WebContent/WEB-INF/view/student-form.jsp
@@ -17,6 +17,15 @@
 		Last name: <form:input path="lastName" />
 		
 		<br><br>
+
+		<form:select path="country">
+
+			<form:options items="${student.countryOptions}" />
+
+
+		</form:select>
+
+		<br><br>
 		
 		<input type="submit" value="Submit">
 	</form:form>
--- src/com/zhouyi2019/springdemo/mvc/Student.java
+++ src/com/zhouyi2019/springdemo/mvc/Student.java
@@ -1,12 +1,24 @@
 package com.zhouyi2019.springdemo.mvc;
 
+import java.util.LinkedHashMap;
+
 public class Student {
 	
 	private String firstName;
 	private String lastName;
-	
+	private String Country;
+	private LinkedHashMap<String, String> countryOptions;
 	public Student() {
-		
+		// populate country options: used ISO country code
+
+		countryOptions = new LinkedHashMap<>();
+
+		countryOptions.put("BR", "Brazil");
+		countryOptions.put("CN", "CHina");
+		countryOptions.put("DE", "Germany");
+		countryOptions.put("JP", "Japan");
+		countryOptions.put("US", "United States");
+
 	}
 
 	public String getFirstName() {
@@ -24,7 +36,17 @@ public class Student {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	
-	
+
+	public String getCountry() {
+		return Country;
+	}
+
+	public void setCountry(String country) {
+		Country = country;
+	}
+
+	public LinkedHashMap<String, String> getCountryOptions() {
+		return countryOptions;
+	}
 
 }
